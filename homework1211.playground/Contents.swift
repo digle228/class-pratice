93c6534d0cd5b7138ba0982a523dd033b2ea5fa8
wusu chiang <wusuchen@wusudeiMac.local>
2016-01-04 15:07:47 +0800

    ready

---
+++ homework1211.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+for num in 1...5  {
+
+    var message = "I am fine"
+
+}
+var add = 1
+for add in 1...99 {
+var n = 1
+    n = n++
+var total = n * n
+total = total + total
+total
+}
+
---
+++ test.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+
+
+var t: Double
+var e: Double
+
+func count(amount: String?, tage: String?) ->() {
+     let t = Double(amount!)
+     let e = Double(tage!)
+    
+    t! * e!
+    return
+
+}
+
+count("0.2", tage: "0.6")
+
+func count1() {
+    print("hallo")
+    
+}
+count1()
